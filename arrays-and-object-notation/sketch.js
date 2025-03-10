e35731b0f54af7b9030d2d7d1c1413b7a5fc2d27
Gitaton <1302484@spsd.sk.ca>
2025-03-10 14:14:44 -0600

    Added Comments
    Added Comments for what to do in the future (TODO list), and added to the comment header

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -1,9 +1,12 @@
-// Project Title
-// Your Name
+// Wave defense 2D but multiplayer like clash royale or something | Dino Bash 
+// Karthik Narayan
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+// TODO 
+// - Add p5.party
 
 
 function setup() {
