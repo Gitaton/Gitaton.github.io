fdccfd63b08133e0ee3c915d7515017e00704b81
Gitaton <1302484@spsd.sk.ca>
2025-03-12 14:16:20 -0600

    Attempted to add p5.party to my project

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -1,6 +1,6 @@
 // Wave defense 2D but multiplayer like clash royale or something | Dino Bash 
 // Karthik Narayan
-// Date
+// Mar.12, 2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,11 +8,33 @@
 // TODO 
 // - Add p5.party
 
+function preload() {
+  // Connect to the server
+  //partyConnect("wss://demoserver.p5party.org", "battleMans");
+
+  // Loads shared objects
+  // shared = partyLoadShared("shared", {
+  //   x: 50,
+  //   y: 50,
+  // });
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // Show p5.party panel
+  //partyToggleInfo(true);
+}
+
+function mousePressed() {
+  // shared.x = mouseX;
+  // shared.y = mouseY;
 }
 
 function draw() {
   background(220);
+
+  fill("black");
+  
+  rect(100, 100, 50, 50);
 }
