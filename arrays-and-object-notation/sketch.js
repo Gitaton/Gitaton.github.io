896c1f5d14aa02413139cfd028dd26f4ed6fd70d
Gitaton <1302484@spsd.sk.ca>
2025-03-13 12:15:52 -0600

    p5.party now working, and client side shared variables now work

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -8,33 +8,41 @@
 // TODO 
 // - Add p5.party
 
+let shared;
+
 function preload() {
   // Connect to the server
-  //partyConnect("wss://demoserver.p5party.org", "battleMans");
+  partyConnect("wss://demoserver.p5party.org", "battleMans");
 
   // Loads shared objects
-  // shared = partyLoadShared("shared", {
-  //   x: 50,
-  //   y: 50,
-  // });
+  guests = partyLoadGuestShareds();
+
+  me = partyLoadMyShared({
+    x: 50,
+    y: 50,
+  });
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // Show p5.party panel
-  //partyToggleInfo(true);
-}
-
-function mousePressed() {
-  // shared.x = mouseX;
-  // shared.y = mouseY;
+  partyToggleInfo(true);
 }
 
 function draw() {
   background(220);
+  player();
+  console.log(me.x);
+}
 
+function player() {
   fill("black");
-  
-  rect(100, 100, 50, 50);
+  rect(me.x, me.y, 50, 50);
+  if (keyIsDown(68)) {
+    me.x += 1;
+  }
+  if (keyIsDown(65)) {
+    me.x-= 1;
+  }
 }
