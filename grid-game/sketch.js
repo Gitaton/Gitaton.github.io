8acaa9b1c830c3879e1d659d58fd9a8f2eca9cda
Gitaton <1302484@spsd.sk.ca>
2025-03-31 14:16:28 -0600

    Worked on deleting redundant players, then added a enemy spawner and end location. I need to add pathfinding next for the enemy path.

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -4,14 +4,25 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+// TODO:
+// - PATHFINDING - TRY BFS PATHFINDING FIRST
 
 const CELL_SIZE = 100;
-const PLAYER = 2;
+const PLAYER = "P";
+const BALLOON_START = "S";
+const BALLOON_END = "E";
 let grid;
 let rows;
 let cols;
 let oldY;
 let oldX;
+
+let playerPiece = {
+  x: 0,
+  y: 0,
+  oldX: 0, 
+  oldY: 0,
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,11 +31,13 @@ function setup() {
   rows = height/CELL_SIZE;
 
   grid = generateGridMap(cols, rows);
+  balloonStartAndEnd();
 }
 
 function draw() {
   background(220);
   displayGrid(cols, rows);
+  displayPiece();
 }
 
 function generateGridMap(cols, rows) {
@@ -41,26 +54,54 @@ function generateGridMap(cols, rows) {
 function displayGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 1) {
-        fill("blue");
-      }
-      else if (grid[y][x] === 0) {
-        fill("red");
+      if (grid[y][x] === 0) {
+        fill("orangered");
       }
       else if (grid[y][x] === PLAYER) {
         fill("white");
+      }
+      else if (grid[y][x] === BALLOON_START) {
+        fill("green");
+      }
+      else if (grid[y][x] === BALLOON_END) {
+        fill("black");
       }
       rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
     }
   }
 }
 
-function mouseClicked() {
-  // Find the x and y positions relative to the grid
-  let y = Math.ceil(mouseY/CELL_SIZE) - 1;
-  let x = Math.ceil(mouseX/CELL_SIZE) - 1;
+function balloonStartAndEnd() {
+  // Create a balloon spawn point
+  let balloonSpawnLocation = {
+    x: Math.floor(random(cols/2)),
+    y: Math.floor(random(rows)),
+  };
 
+  // Create a balloon end point
+  let balloonEndLocation = {
+    x: Math.floor(random(cols/2, cols)),
+    y: Math.floor(random(rows)),
+  };
+  grid[balloonSpawnLocation.y][balloonSpawnLocation.x] = BALLOON_START;
+  grid[balloonEndLocation.y][balloonEndLocation.x] = BALLOON_END;
+}
+
+function displayPiece() {
   // Change the tile to the player
-  grid[y][x] = PLAYER;
-  console.log(x, y);
+  grid[playerPiece.y][playerPiece.x] = PLAYER;
+  console.log(playerPiece.x, playerPiece.y);
+
+  // Set old piece location
+  playerPiece.oldY = playerPiece.y;
+  playerPiece.oldX = playerPiece.x;
+}
+
+function mouseClicked() {
+  // Clear old piece
+  grid[playerPiece.oldY][playerPiece.oldX] = 0;
+
+  // Find the x and y positions relative to the grid
+  playerPiece.y = Math.ceil(mouseY/CELL_SIZE) - 1;
+  playerPiece.x = Math.ceil(mouseX/CELL_SIZE) - 1;
 }
